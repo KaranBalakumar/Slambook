5d7178b99b98b85226cebd0405880a1c9630dcdf
Uchiha729 <f20221736@hyderabad.bits-pilani.ac.in>
2024-06-03 14:58:23 +0530

    Adding first README

---
+++ Eigen3AndPangolin/README.md
@@ -0,0 +1,5 @@
+# Visual Odometry and VisualSLAM basics in C++
+This repository contains basics and implementations required for Visual Odometry and VisualSLAM. This repository contains,
+- Very Basic C++ and CMake examples.
+- implementation of ```Eigen3``` template library for linear algebra: matrices, vectors, numerical solvers, and related algorithms.
+- Simple 3D Trajectory plotter using ```Eigen3``` and ```Pangolin```.
--- Eigen3AndPangolin/examples/plotTrajectory.cpp
+++ Eigen3AndPangolin/examples/plotTrajectory.cpp
@@ -7,7 +7,7 @@ using namespace std;
 using namespace Eigen;
 
 // path to trajectory file
-string trajectory_file = "/home/karan/slambook/ch3/examples/trajectory.txt";
+string trajectory_file = "/home/karan/slambook/Eigen3AndPangolin/examples/trajectory.txt";
 
 void DrawTrajectory(vector<Isometry3d, Eigen::aligned_allocator<Isometry3d>>);
 
@@ -37,19 +37,21 @@ int main(int argc, char **argv) {
 /*******************************************************************************************/
 void DrawTrajectory(vector<Isometry3d, Eigen::aligned_allocator<Isometry3d>> poses) {
   // create pangolin window and plot the trajectory
-  pangolin::CreateWindowAndBind("Trajectory Viewer", 1024, 768);
-  glEnable(GL_DEPTH_TEST);
-  glEnable(GL_BLEND);
-  glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+  pangolin::CreateWindowAndBind("Trajectory Viewer", 1024, 768); // window size
+  glEnable(GL_DEPTH_TEST); // to remove hidden surface by enabling depth-buffer
+  glEnable(GL_BLEND); // enables blending which could be used to implement transperency within objects
+  glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA); // func to assign values to C_source  
+                                                     // and C_destinationin blend equation
 
   pangolin::OpenGlRenderState s_cam(
     pangolin::ProjectionMatrix(1024, 768, 500, 500, 512, 389, 0.1, 1000),
     pangolin::ModelViewLookAt(0, -0.1, -1.8, 0, 0, 0, 0.0, -1.0, 0.0)
-  );
+  ); // setting up camera state by passing projection matrix and 
+     // ModelViewLookAt(where camera is?, where object is?, where camera is looking?)
 
   pangolin::View &d_cam = pangolin::CreateDisplay()
     .SetBounds(0.0, 1.0, 0.0, 1.0, -1024.0f / 768.0f)
-    .SetHandler(new pangolin::Handler3D(s_cam));
+    .SetHandler(new pangolin::Handler3D(s_cam)); // similarly setting up display cam parameters
 
   while (pangolin::ShouldQuit() == false) {
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
@@ -59,18 +61,18 @@ void DrawTrajectory(vector<Isometry3d, Eigen::aligned_allocator<Isometry3d>> pos
     for (size_t i = 0; i < poses.size(); i++) {
       //Draw Poses
       Vector3d Ow = poses[i].translation();
-      Vector3d Xw = poses[i] * (0.1 * Vector3d(1, 0, 0));
-      Vector3d Yw = poses[i] * (0.1 * Vector3d(0, 1, 0));
-      Vector3d Zw = poses[i] * (0.1 * Vector3d(0, 0, 1));
+      Vector3d Xw = poses[i] * (0.1 * Vector3d(1, 0, 0)); //getting x-axis
+      Vector3d Yw = poses[i] * (0.1 * Vector3d(0, 1, 0)); //getting y-axis
+      Vector3d Zw = poses[i] * (0.1 * Vector3d(0, 0, 1)); // getting z-axis
       glBegin(GL_LINES);
-      glColor3f(1.0, 0.0, 0.0);
-      glVertex3d(Ow[0], Ow[1], Ow[2]);
+      glColor3f(1.0, 0.0, 0.0); // setting red color
+      glVertex3d(Ow[0], Ow[1], Ow[2]); // Drawing line from camera origin towards x-axis in Camera's coordinate system
       glVertex3d(Xw[0], Xw[1], Xw[2]);
-      glColor3f(0.0, 1.0, 0.0);
-      glVertex3d(Ow[0], Ow[1], Ow[2]);
+      glColor3f(0.0, 1.0, 0.0); // setting green color
+      glVertex3d(Ow[0], Ow[1], Ow[2]); // Drawing line from camera origin towards y-axis in Camera's coordinate system
       glVertex3d(Yw[0], Yw[1], Yw[2]);
-      glColor3f(0.0, 0.0, 1.0);
-      glVertex3d(Ow[0], Ow[1], Ow[2]);
+      glColor3f(0.0, 0.0, 1.0); // setting blue color
+      glVertex3d(Ow[0], Ow[1], Ow[2]);// Drawing line from camera origin towards z-axis in Camera's coordinate system
       glVertex3d(Zw[0], Zw[1], Zw[2]);
       glEnd();
     }
